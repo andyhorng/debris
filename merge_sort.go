2935cf29200e224634cd41666ba9def2928a9c1a
洪盟翔 <andy@eztable.com.tw>
2014-11-16 15:04:52 +0800

    Non-parallel version

---
+++ merge_sort.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	//"fmt"
+	"math/rand"
+)
+
+func mergeSort(list []int) []int {
+
+	if len(list) <= 1 {
+		return list
+	} else if len(list) <= 2 {
+		if list[0] > list[1] {
+			list[0], list[1] = list[1], list[0]
+		}
+
+		return list
+	} else {
+
+		half := len(list) / 2
+
+		// divide
+		a := mergeSort(list[0:half])
+		b := mergeSort(list[half:])
+
+		buf := make([]int, len(list))
+
+		i, j := 0, 0
+
+		for i < len(a) && j < len(b) {
+			if a[i] < b[j] {
+				buf[i+j] = a[i]
+				i += 1
+			} else {
+				buf[i+j] = b[j]
+				j += 1
+			}
+		}
+
+		for k, v := range a[i:] {
+			buf[i+j+k] = v
+		}
+
+		for k, v := range b[j:] {
+			buf[i+j+k] = v
+		}
+
+		// conquer
+		return buf
+	}
+}
+
+func main() {
+	list := rand.Perm(10)
+	mergeSort(list)
+}
